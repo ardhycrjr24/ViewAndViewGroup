842338618174adcfe9fd24197403a22c1c0b64c7
SMC <@ardhycrjr24>
2026-04-24 02:24:45 +0700

    05styleandtheme

---
+++ app/src/main/java/id/ac/smpn8bks/ardiansyah/styleandtheme/MainActivity.kt
@@ -0,0 +1,25 @@
+package id.ac.smpn8bks.ardiansyah.styleandtheme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+<ScrollView
+android:layout_width="match_parent"
+android:layout_height="match_parent">
+
+<LinearLayout
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:orientation="vertical">
+
+</LinearLayout>
+
+</ScrollView>
